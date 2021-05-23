3f499dae84cfc80b2869b69e07304b1091d07455
Simran <simran5590@gmail.com>
2021-05-23 13:59:05 -0700

    Order and Order Routes and a different way of Routing

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,6 +1,9 @@
 package com.jetbrains.handson.httpapi
 
 import com.jetbrains.handson.httpapi.routes.customerRouting
+import com.jetbrains.handson.httpapi.routes.getOrderRoute
+import com.jetbrains.handson.httpapi.routes.listOrderRoute
+import com.jetbrains.handson.httpapi.routes.totalizeOrderRoute
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.routing.*
@@ -15,5 +18,14 @@ fun Application.module() {
     }
     install(Routing) {
         customerRouting()
+    }
+    registerOrderRoutes()
+}
+
+fun Application.registerOrderRoutes() {
+    routing {
+        listOrderRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
